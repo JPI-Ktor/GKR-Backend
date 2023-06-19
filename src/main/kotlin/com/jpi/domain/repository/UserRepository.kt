695fb07ff1ea6c522cad8c6760b17826e9857879
hyeonseo <s21035@gsm.hs.kr>
2023-06-19 20:34:00 +0900

    :sparkles: :: 대여 제재 domain 로직

--- src/main/kotlin/com/jpi/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/jpi/domain/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package com.jpi.domain.repository
 
 import com.jpi.domain.model.response.UserResponse
+import java.util.UUID
 
 interface UserRepository {
     suspend fun getStudent(email: String): UserResponse?
 
     suspend fun getAllStudents(): List<UserResponse>
+
+    suspend fun restrictRental(id: UUID)
 }
