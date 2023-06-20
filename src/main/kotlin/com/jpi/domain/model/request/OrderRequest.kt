20c1d9033862854de353825a4e42f98f91d78064
박성현 <pjhomg5530@naver.com>
2023-06-20 20:12:03 +0900

    :recycle: 속성들 수정하기

--- src/main/kotlin/com/jpi/domain/model/request/OrderRequest.kt
+++ src/main/kotlin/com/jpi/domain/model/request/OrderRequest.kt
@@ -1,13 +1,15 @@
 package com.jpi.domain.model.request
 
 import com.jpi.domain.State
+import com.jpi.domain.model.util.UUIDSerializer
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class OrderRequest(
-    val userId: String,
+    @Serializable(with = UUIDSerializer::class)
+    val userId: UUID,
 //    val id: String,
     val reason: String,
     val state: State,
-    val date: String
 )
