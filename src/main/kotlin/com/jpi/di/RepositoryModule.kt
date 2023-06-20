56edf77d3f5cc228a573e14c041620e07cc27810
박성현 <pjhomg5530@naver.com>
2023-06-20 20:13:59 +0900

    :sparkles: OrderRepository DI 세팅

--- src/main/kotlin/com/jpi/di/RepositoryModule.kt
+++ src/main/kotlin/com/jpi/di/RepositoryModule.kt
@@ -1,12 +1,15 @@
 package com.jpi.di
 
 import com.jpi.data.repository.AuthRepositoryImpl
+import com.jpi.data.repository.OrderRepositoryImpl
 import com.jpi.data.repository.UserRepositoryImpl
 import com.jpi.domain.repository.AuthRepository
+import com.jpi.domain.repository.OrderRepository
 import com.jpi.domain.repository.UserRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<UserRepository> { UserRepositoryImpl(get()) }
     single<AuthRepository> { AuthRepositoryImpl(get()) }
+    single<OrderRepository> { OrderRepositoryImpl() }
 }
