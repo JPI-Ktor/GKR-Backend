02cf364f06c74c35ffb9dc849a07b8eecced3fee
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-06-20 20:25:59 +0900

    :recycle: image text로 변경

--- src/main/kotlin/com/jpi/domain/entity/Equipment.kt
+++ src/main/kotlin/com/jpi/domain/entity/Equipment.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.Table
 object Equipment: Table() {
     val productNumber = varchar("productNumber", 20)
     val name = varchar("name", 25)
-    val image = varchar("image", 500)
+    val image = text("image")
     val description = varchar("description", 200)
     val numberLeft = integer("numberLeft")
     val rentStatus = enumerationByName("rentStatus", 10, RentStatus::class)
