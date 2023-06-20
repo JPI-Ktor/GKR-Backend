6271386186b6fdef44b05fa3f8db1315da727ca2
박성현 <pjhomg5530@naver.com>
2023-06-20 20:12:23 +0900

    :sparkles: OrderRepositoryImpl 생성

---
+++ src/main/kotlin/com/jpi/data/repository/OrderRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package com.jpi.data.repository
+
+import com.jpi.data.model.response.asOrderResponse
+import com.jpi.domain.State
+import com.jpi.domain.entity.Order
+import com.jpi.domain.model.request.ExtensionRequest
+import com.jpi.domain.model.request.OrderRequest
+import com.jpi.domain.model.response.OrderResponse
+import com.jpi.domain.repository.OrderRepository
+import com.jpi.server.DatabaseFactory.dbQuery
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.update
+import java.time.LocalDateTime
+
+class OrderRepositoryImpl: OrderRepository {
+    override suspend fun postRentalRequest(orderRequest: OrderRequest): Unit = dbQuery {
+        Order.insert {
+            it[this.userId] = orderRequest.userId
+            it[this.rentalReason] = orderRequest.reason
+            it[this.rentalDate] = LocalDateTime.now().plusMonths(1)
+            it[this.rentalState] = State.RENTAL_STATE
+        }
+    }
+
+    override suspend fun postReturnRequest(orderRequest: OrderRequest): Unit = dbQuery {
+        Order.insert {
+            it[this.userId] = orderRequest.userId
+            it[this.rentalReason] = orderRequest.reason
+            it[this.rentalDate] = LocalDateTime.now().plusMonths(1)
+            it[this.rentalState] = State.RETURN_STATE
+        }
+    }
+
+    override suspend fun postExtensionRequest(extensionRequest: ExtensionRequest) {
+        val date = Order.select { Order.userId eq extensionRequest.userId }.map { it[Order.rentalDate] }.single()
+
+        Order.update({ Order.userId eq extensionRequest.userId }) {
+            it[this.rentalDate] = date.plusMonths(1)
+        }
+    }
+
+    override suspend fun getRentalRequestList(): List<OrderResponse> = dbQuery {
+        Order.select { Order.rentalState eq State.RENTAL_STATE }.map { it.asOrderResponse() }
+    }
+
+    override suspend fun getReturnRequestList(): List<OrderResponse> = dbQuery {
+        Order.select { Order.rentalState eq State.RETURN_STATE }.map { it.asOrderResponse() }
+    }
+}
