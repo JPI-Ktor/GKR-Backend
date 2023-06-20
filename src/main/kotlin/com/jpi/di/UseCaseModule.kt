eb4342668843d511652baa29fa97c844dc3f235f
박성현 <pjhomg5530@naver.com>
2023-06-21 08:49:52 +0900

    :sparkles: UseCase DI 세팅

--- src/main/kotlin/com/jpi/di/UseCaseModule.kt
+++ src/main/kotlin/com/jpi/di/UseCaseModule.kt
@@ -4,6 +4,7 @@ import com.jpi.domain.usecase.auth.GetEmailByTokenUseCase
 import com.jpi.domain.usecase.auth.IsTokenValidUseCase
 import com.jpi.domain.usecase.auth.ReissueTokenUseCase
 import com.jpi.domain.usecase.auth.SignInUseCase
+import com.jpi.domain.usecase.order.*
 import com.jpi.domain.usecase.user.*
 import org.koin.dsl.module
 
@@ -20,4 +21,11 @@ val useCaseModule = module {
     single { SignInUseCase(get()) }
     single { GetEmailByTokenUseCase(get()) }
     single { IsTokenValidUseCase(get()) }
+
+    // Order
+    single { GetRentalRequestListUseCase(get()) }
+    single { GetReturnRequestListUseCase(get()) }
+    single { PostRentalRequestUseCase(get()) }
+    single { PostReturnRequestUseCase(get()) }
+    single { PostExtensionRequestUseCase(get()) }
 }
