fdfb21bdceba9b56f99e500d1afb8c904ed1caa4
hyeonseo <s21035@gsm.hs.kr>
2023-06-20 10:31:58 +0900

    :sparkles: :: 로그아웃 api 로직

--- src/main/kotlin/com/jpi/api/UserApi.kt
+++ src/main/kotlin/com/jpi/api/UserApi.kt
@@ -2,9 +2,7 @@ package com.jpi.api
 
 import com.jpi.data.model.request.UserRequest
 import com.jpi.domain.usecase.auth.GetEmailByTokenUseCase
-import com.jpi.domain.usecase.user.GetAllStudentUseCase
-import com.jpi.domain.usecase.user.GetStudentUseCase
-import com.jpi.domain.usecase.user.RestrictRentalUseCase
+import com.jpi.domain.usecase.user.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -18,6 +16,8 @@ fun Route.userRoute() {
     val getAllStudentUseCase: GetAllStudentUseCase by inject()
     val getEmailByTokenUseCase: GetEmailByTokenUseCase by inject()
     val restrictRentalUseCase: RestrictRentalUseCase by inject()
+    val getUUIDUseCase: GetUUIDUseCase by inject()
+    val logoutUseCase: LogoutUseCase by inject()
 
     val tokenPrefix = "Bearer "
 
@@ -48,5 +48,21 @@ fun Route.userRoute() {
         )
         restrictRentalUseCase(id = UUID.fromString(userRequest.id))
         call.respondText(status = HttpStatusCode.OK, text = "학생을 제재하였습니다.")
+    }
+    delete("user/logout") {
+        val accessToken = call.request.headers[HttpHeaders.Authorization] ?: return@delete call.respondText(
+            status = HttpStatusCode.BadRequest,
+            text = "잘못된 요청입니다."
+        )
+        if (!accessToken.startsWith(tokenPrefix)) call.respondText(
+            status = HttpStatusCode.Unauthorized,
+            text = "유효하지 않은 토큰입니다."
+        )
+        val uuid = getUUIDUseCase(accessToken) ?: return@delete call.respondText(
+            status = HttpStatusCode.NotFound,
+            text = "유저를 찾을 수 없습니다."
+        )
+        logoutUseCase(uuid)
+        call.respondText(status = HttpStatusCode.OK, text = "로그아웃 하였습니다.")
     }
 }
