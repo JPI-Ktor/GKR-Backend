d5b75e3073a7d9e81a587da5adef594ffe568119
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-06-20 19:50:46 +0900

    :sparkles: equipment 테이블 생성

---
+++ src/main/kotlin/com/jpi/domain/entity/Equipment.kt
@@ -0,0 +1,15 @@
+package com.jpi.domain.entity
+
+import com.jpi.domain.util.RentStatus
+import org.jetbrains.exposed.sql.Table
+
+object Equipment: Table() {
+    val productNumber = varchar("productNumber", 20)
+    val name = varchar("name", 25)
+    val image = varchar("image", 500)
+    val description = varchar("description", 200)
+    val numberLeft = integer("numberLeft")
+    val rentStatus = enumerationByName("rentStatus", 10, RentStatus::class)
+
+    override val primaryKey = PrimaryKey(productNumber)
+}
