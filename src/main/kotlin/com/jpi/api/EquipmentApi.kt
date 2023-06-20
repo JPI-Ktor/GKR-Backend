c52c063eedf917c3e627240c38347169119507b2
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-06-20 20:57:31 +0900

    :recycle: 중복된 path 묶기

--- src/main/kotlin/com/jpi/api/EquipmentApi.kt
+++ src/main/kotlin/com/jpi/api/EquipmentApi.kt
@@ -17,51 +17,53 @@ fun Route.equipmentRoute() {
     val addEquipmentUseCase: AddEquipmentUseCase by inject()
     val deleteEquipmentUseCase: DeleteEquipmentUseCase by inject()
 
-    get("/equipment/all") {
-        val getAll = getAllEquipmentUseCase()
+    route("/equipment") {
+        get("/all") {
+            val getAll = getAllEquipmentUseCase()
 
-        if (getAll.isEmpty()) {
-            return@get call.respondText(status = HttpStatusCode.NotFound, text = "기자재가 존재하지 않습니다.")
-        }
-        call.respond(status = HttpStatusCode.OK, message = getAll)
-    }
-
-    post("/equipment") {
-        val equipment = call.receiveNullable<EquipmentRequest>()
-            ?: return@post call.respondText(text = "잘못된 요청입니다.", status = HttpStatusCode.BadRequest)
-
-        val addEquipment = addEquipmentUseCase(equipment)
-
-        call.respond(status = HttpStatusCode.Created, message = addEquipment)
-    }
-
-    get("/equipment/notrent") {
-        val getNotRent = getNotRentEquipmentUseCase()
-
-        call.respond(status = HttpStatusCode.OK, message = getNotRent)
-    }
-
-    get("/equipment/isrent") {
-        val getIsRent = getIsRentEquipmentUseCase()
-
-        call.respond(status = HttpStatusCode.OK, message = getIsRent)
-    }
-
-    get("/equipment/{productNumber}") {
-        val productNumber = call.parameters["productNumber"].toString()
-        val equipmentInfo = equipmentUseCase(productNumber)
-            ?: return@get call.respondText(text = "없는 기자재 입니다.", status = HttpStatusCode.NotFound)
-
-        call.respond(status = HttpStatusCode.OK, message = equipmentInfo)
-    }
-
-    delete("/equipment/{productNumber}") {
-        val productNum = call.parameters["productNumber"].toString()
-        val deleteEquipment = deleteEquipmentUseCase(productNum)
-        if (!deleteEquipment) {
-            return@delete call.respondText(status = HttpStatusCode.NotFound, text = "없는 기자재 입니다.")
+            if (getAll.isEmpty()) {
+                return@get call.respondText(status = HttpStatusCode.NotFound, text = "기자재가 존재하지 않습니다.")
+            }
+            call.respond(status = HttpStatusCode.OK, message = getAll)
         }
 
-        call.respondText(status = HttpStatusCode.NoContent, text = "삭제 완료")
+        post {
+            val equipment = call.receiveNullable<EquipmentRequest>()
+                    ?: return@post call.respondText(text = "잘못된 요청입니다.", status = HttpStatusCode.BadRequest)
+
+            val addEquipment = addEquipmentUseCase(equipment)
+
+            call.respond(status = HttpStatusCode.Created, message = addEquipment)
+        }
+
+        get("/notrent") {
+            val getNotRent = getNotRentEquipmentUseCase()
+
+            call.respond(status = HttpStatusCode.OK, message = getNotRent)
+        }
+
+        get("/isrent") {
+            val getIsRent = getIsRentEquipmentUseCase()
+
+            call.respond(status = HttpStatusCode.OK, message = getIsRent)
+        }
+
+        get("/{productNumber}") {
+            val productNumber = call.parameters["productNumber"].toString()
+            val equipmentInfo = equipmentUseCase(productNumber)
+                    ?: return@get call.respondText(text = "없는 기자재 입니다.", status = HttpStatusCode.NotFound)
+
+            call.respond(status = HttpStatusCode.OK, message = equipmentInfo)
+        }
+
+        delete("/{productNumber}") {
+            val productNum = call.parameters["productNumber"].toString()
+            val deleteEquipment = deleteEquipmentUseCase(productNum)
+            if (!deleteEquipment) {
+                return@delete call.respondText(status = HttpStatusCode.NotFound, text = "없는 기자재 입니다.")
+            }
+
+            call.respondText(status = HttpStatusCode.NoContent, text = "삭제 완료")
+        }
     }
 }
