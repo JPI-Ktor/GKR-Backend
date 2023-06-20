5749fc43ef56c93181adfc1d17233f4fa877e4b7
박성현 <pjhomg5530@naver.com>
2023-06-20 18:08:07 +0900

    :sparkles: state 변수 추가

--- src/main/kotlin/com/jpi/domain/model/request/OrderRequest.kt
+++ src/main/kotlin/com/jpi/domain/model/request/OrderRequest.kt
@@ -1,10 +1,12 @@
 package com.jpi.domain.model.request
 
+import com.jpi.domain.State
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class OrderRequest(
     val userId: String,
 //    val id: String,
-    val reason: String
+    val reason: String,
+    val state: State
 )
--- src/main/kotlin/com/jpi/domain/model/response/OrderResponse.kt
+++ src/main/kotlin/com/jpi/domain/model/response/OrderResponse.kt
@@ -1,7 +1,10 @@
 package com.jpi.domain.model.response
 
+import com.jpi.domain.State
+
 data class OrderResponse(
     val userId: String,
 //    val id: String,
-    val reason: String
+    val reason: String,
+    val state: State
 )
