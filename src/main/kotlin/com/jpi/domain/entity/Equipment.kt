b94c1bb3de8a273dcf27321b8314d403f876ae5a
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-06-21 16:04:11 +0900

    :fire: table 남은 개수 item 삭제

--- src/main/kotlin/com/jpi/domain/entity/Equipment.kt
+++ src/main/kotlin/com/jpi/domain/entity/Equipment.kt
@@ -8,7 +8,6 @@ object Equipment: Table() {
     val name = varchar("name", 25)
     val image = text("image")
     val description = varchar("description", 200)
-    val numberLeft = integer("number_left")
     val rentStatus = enumerationByName("rent_status", 10, RentStatus::class)
 
     override val primaryKey = PrimaryKey(productNumber)
