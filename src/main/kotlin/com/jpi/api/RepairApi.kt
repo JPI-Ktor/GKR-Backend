194bd353db80119d918d3efc333ca6e7fd6f0ec2
hyeonseo <s21035@gsm.hs.kr>
2023-06-21 16:57:54 +0900

    :sparkles: :: 수리 내역 api 로직

---
+++ src/main/kotlin/com/jpi/api/RepairApi.kt
@@ -0,0 +1,69 @@
+package com.jpi.api
+
+import com.jpi.api.util.getAccessToken
+import com.jpi.data.model.request.RepairRequestData
+import com.jpi.domain.model.request.RepairRequest
+import com.jpi.domain.usecase.auth.IsTokenValidUseCase
+import com.jpi.domain.usecase.repair.AddRepairHistoryUseCase
+import com.jpi.domain.usecase.repair.DeleteRepairHistoryUseCase
+import com.jpi.domain.usecase.repair.GetRepairHistoryUseCase
+import com.jpi.domain.usecase.repair.ModifyRepairHistoryUseCase
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Route.repairRoute() {
+    val addRepairHistoryUseCase: AddRepairHistoryUseCase by inject()
+    val modifyRepairHistoryUseCase: ModifyRepairHistoryUseCase by inject()
+    val deleteRepairHistoryUseCase: DeleteRepairHistoryUseCase by inject()
+    val isTokenValidUseCase: IsTokenValidUseCase by inject()
+    val getRepairHistoryUseCase: GetRepairHistoryUseCase by inject()
+
+    route("repair") {
+        post {
+            getAccessToken { isTokenValidUseCase(it) } ?: return@post
+
+            val repairRequest = call.receiveNullable<RepairRequest>() ?: return@post call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "잘못된 요청입니다."
+            )
+            addRepairHistoryUseCase(repairRequest = repairRequest)
+            call.respondText(status = HttpStatusCode.Created, text = "수리 내역을 정상적으로 추가하였습니다.")
+        }
+        get {
+            getAccessToken { isTokenValidUseCase(it) } ?: return@get
+
+            val repairRequest = call.receiveNullable<RepairRequestData>() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "잘못된 요청입니다."
+            )
+            val repairHistory = getRepairHistoryUseCase(productNumber = repairRequest.productNumber)
+                ?: return@get call.respondText(status = HttpStatusCode.NotFound, text = "수리 내역을 찾지 못했습니다.")
+
+            call.respond(status = HttpStatusCode.OK, repairHistory)
+        }
+        patch {
+            getAccessToken { isTokenValidUseCase(it) } ?: return@patch
+
+            val repairRequest = call.receiveNullable<RepairRequest>() ?: return@patch call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "잘못된 요청입니다."
+            )
+            modifyRepairHistoryUseCase(repairRequest = repairRequest)
+            call.respondText(status = HttpStatusCode.OK, text = "수리 내역을 수정하였습니다.")
+        }
+        delete {
+            getAccessToken { isTokenValidUseCase(it) } ?: return@delete
+
+            val repairRequest = call.receiveNullable<RepairRequestData>() ?: return@delete call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "잘못된 요청입니다."
+            )
+            deleteRepairHistoryUseCase(productNumber = repairRequest.productNumber)
+            call.respondText(status = HttpStatusCode.OK, text = "수리 내역을 성공적으로 삭제하였습니다.")
+        }
+    }
+}
