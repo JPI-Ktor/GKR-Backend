944adeea3b79a72569c2679390ce5b784de8b98e
박성현 <pjhomg5530@naver.com>
2023-06-21 14:47:11 +0900

    :recycle: 하멜케이스 -> 스네이크 방식으로 이름 변경

--- src/main/kotlin/com/jpi/domain/entity/Equipment.kt
+++ src/main/kotlin/com/jpi/domain/entity/Equipment.kt
@@ -4,12 +4,12 @@ import com.jpi.domain.util.RentStatus
 import org.jetbrains.exposed.sql.Table
 
 object Equipment: Table() {
-    val productNumber = varchar("productNumber", 20)
+    val productNumber = varchar("product_number", 20)
     val name = varchar("name", 25)
     val image = text("image")
     val description = varchar("description", 200)
-    val numberLeft = integer("numberLeft")
-    val rentStatus = enumerationByName("rentStatus", 10, RentStatus::class)
+    val numberLeft = integer("number_left")
+    val rentStatus = enumerationByName("rent_status", 10, RentStatus::class)
 
     override val primaryKey = PrimaryKey(productNumber)
 }
