d9f489520995b197002fab4153dadadd349817ad
hyeonseo <s21035@gsm.hs.kr>
2023-06-22 17:37:31 +0900

    :sparkles: :: 학생 제재 data 로직

---
+++ src/main/kotlin/com/jpi/data/repository/ViolationRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.jpi.data.repository
+
+import com.jpi.data.model.response.asViolationResponse
+import com.jpi.domain.entity.Violation
+import com.jpi.domain.model.request.ViolationRequest
+import com.jpi.domain.model.response.ViolationResponse
+import com.jpi.domain.repository.ViolationRepository
+import org.jetbrains.exposed.sql.select
+import com.jpi.server.DatabaseFactory.dbQuery
+import org.jetbrains.exposed.sql.insert
+import java.time.LocalDate
+import java.util.*
+
+class ViolationRepositoryImpl: ViolationRepository {
+    override suspend fun rentalViolation(violationRequest: ViolationRequest): Unit = dbQuery {
+        Violation.insert {
+            it[userId] = violationRequest.userId
+            it[reason] = violationRequest.reason
+            it[date] = LocalDate.parse(violationRequest.date)
+        }
+    }
+
+    override suspend fun getViolation(id: UUID): List<ViolationResponse> = dbQuery {
+        Violation.select { Violation.userId eq id }.map { it.asViolationResponse() }
+    }
+}
--- src/main/kotlin/com/jpi/di/RepositoryModule.kt
+++ src/main/kotlin/com/jpi/di/RepositoryModule.kt
@@ -1,15 +1,7 @@
 package com.jpi.di
 
-import com.jpi.data.repository.AuthRepositoryImpl
-import com.jpi.data.repository.OrderRepositoryImpl
-import com.jpi.data.repository.RepairRepositoryImpl
-import com.jpi.data.repository.UserRepositoryImpl
-import com.jpi.data.repository.EquipmentRepositoryImpl
-import com.jpi.domain.repository.AuthRepository
-import com.jpi.domain.repository.OrderRepository
-import com.jpi.domain.repository.RepairRepository
-import com.jpi.domain.repository.UserRepository
-import com.jpi.domain.repository.EquipmentRepository
+import com.jpi.data.repository.*
+import com.jpi.domain.repository.*
 import org.koin.dsl.module
 
 val repositoryModule = module {
@@ -18,4 +10,5 @@ val repositoryModule = module {
     single<RepairRepository> { RepairRepositoryImpl() }
     single<OrderRepository> { OrderRepositoryImpl() }
     single<EquipmentRepository> { EquipmentRepositoryImpl() }
+    single<ViolationRepository> { ViolationRepositoryImpl() }
 }
